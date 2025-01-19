cb4a7af705a911852ca0a602cd5e2ff48c9a7e8b
Yash Vishnoi <88367411+YashVishnoi47@users.noreply.github.com>
2025-01-19 14:24:31 +0530

    Refactor webhook route imports and middleware configuration; add useUnifiedTopology option for mongoose connection

--- middleware.js
+++ middleware.js
@@ -1,24 +1,18 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)'])
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
 export default clerkMiddleware({
   publicRoutes: [
     "/",
-    ".events/:id",
     "/api/webhook/clerk",
-  ],
-  ignoredRoutes: [
-    "/api/webhook/clerk",
-    "/api/uploadthing",
   ],
 });
 
+
+
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
+    // Match all routes except internal Next.js files and static assets
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    '/(api|trpc)(.*)', // Always match API routes
   ],
-}
+};
